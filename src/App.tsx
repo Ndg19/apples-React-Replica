217abb0cf94835b234d6aaf46bf740a45d901218
Ndg19 <158159787+Ndg19@users.noreply.github.com>
2025-08-04 10:20:35 +0300

    Apple riplica with react project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './commonResource/css/styles.css';
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>test</h1>
+    </>
+  )
+}
+
+export default App;
